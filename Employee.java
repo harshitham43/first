f552bf6bf6eddcc38b66a08c0534de3c325c6fe8
Harshitha M <harshitha.mr@sonata-software.com>
2019-08-14 11:19:58 +0530

    A java file which defines employee details

---
+++ Employee.java
@@ -0,0 +1,32 @@
+package emp;
+
+public class Employee {
+	private String  empName;
+	private int empId;
+	private double age;
+	
+	
+	
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId ) {
+		this.empId = empId;
+	}
+	public double getAge() {
+		return age;
+	}
+	public void setAge(double age) {
+		this.age = age;
+	}
+	
+	
+	
+
+}
